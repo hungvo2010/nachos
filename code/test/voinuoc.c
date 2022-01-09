cd99600c65e56dbc3f60531b86f5b63e605f7f4e
NXV <66857046+NXV5111@users.noreply.github.com>
2022-01-09 17:35:51 +0700

    Update voinuoc.c

--- code/test/voinuoc.c
+++ code/test/voinuoc.c
@@ -23,14 +23,5 @@ int main()
     for (i = 0; i < n_sinhvien; ++i)
         Join(pid[i]);
 
-    Read(tmp, 1, 0);
-    n_sinhvien = tmp[0] - '0';
-
-    for (i = 0; i < n_sinhvien; ++i)
-        pid[i] = Exec(sinhvien_execuatable);
-
-    for (i = 0; i < n_sinhvien; ++i)
-        Join(pid[i]);
-
     return 0;
 }
