9b03a80659109b532b8d71ab66b440e8f1ea18ad
hungvo2010 <hungvochanhdp1@gmail.com>
2022-01-22 10:01:19 +0700

    rename function, variables name

--- NachOS-4.0/code/test/sinhvien.c
+++ NachOS-4.0/code/test/sinhvien.c
@@ -1,32 +1,32 @@
 #include "syscall.h"
 
-char outputFile[20] = "output.txt";
-char semaphore_name[10] = "voinuoc";
+char output[20] = "output.txt";
+char voinuoc[10] = "voinuoc";
 char tmp[10];
 char buffer[3] = "  ";
 int main()
 {
-    int fd1;
+    int fileid1;
     int pid;
-    int current_amount = 0;
+    int count = 0;
     int i = 0;
     int rand_num;
     pid = GetPID();
     
 
-    while (current_amount < 10)
+    while (count < 10)
     {
-        Wait(semaphore_name);
-        fd1 = Open(outputFile, 2); // append
+        Wait(voinuoc);
+        fileid1 = Open(output, 2);
 
         buffer[0] = (char)('0' + pid);
         rand_num = RandomNum() % 10000;
         for (i = 0; i < rand_num; ++i);
-	    Write(buffer, 2, fd1);
-        current_amount++;
+	    Write(buffer, 2, fileid1);
+        count++;
 
-        Close(fd1);
-        Signal(semaphore_name);
+        Close(fileid1);
+        Signal(voinuoc);
     } 
     return 0;
 }
--- NachOS-4.0/code/test/voinuoc.c
+++ NachOS-4.0/code/test/voinuoc.c
@@ -1,28 +1,28 @@
 #include "syscall.h"
 
-char inputFile[20] = "input.txt";
-char outputFile[20] = "output.txt";
-char sinhvien_execuatable[20] = "sinhvien";
-char semaphore_name[10] = "voinuoc";
+char input[20] = "input.txt";
+char output[20] = "output.txt";
+char sinhvien_exe[20] = "sinhvien";
+char voinuoc[10] = "voinuoc";
 char tmp[10];
 int pid[10];
 int main() {
-    int fd1, n_sinhvien;
+    int fileid1, n_sinhvien;
     int i = 0;
 
-    CreateSemaphore(semaphore_name, 1);
-    fd1 = Open(inputFile, 0);
-    CreateFile(outputFile);
-    Read(tmp, 1, fd1);  // n < 5
+    CreateSemaphore(voinuoc, 1);
+    fileid1 = Open(input, 0);
+    CreateFile(output);
+    Read(tmp, 1, fileid1);
     n_sinhvien = tmp[0] - '0';
 
     for (i = 0; i < n_sinhvien; ++i)
-        pid[i] = Exec(sinhvien_execuatable);
+        pid[i] = Exec(sinhvien_exe);
 
     for (i = 0; i < n_sinhvien; ++i)
         Join(pid[i]);
 
-    Close(fd1);
+    Close(fileid1);
 
     return 0;
 }
